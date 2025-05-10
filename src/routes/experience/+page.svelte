70d2071552f67b67628301dd2bffc03403dc3980
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-05-10 14:43:13 -0500

    Removed duplicate images

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -13,8 +13,8 @@
 
     <h1>Jobs and Research</h1>
     <div class='content-row'>
-        <div class='project-image-wrapper'>
-            <img src={structurology} class='' alt='Structurology'/>
+        <div class='project-image-wrapper experience'>
+            <img src={structurology} alt='Structurology'/>
         </div>
         <div class='header-text'>
             <p>I worked at Structurology during Summer 2024. I primarily made Microsoft Flows to automate processes for the engineers working at Structurology but I also worked on some tools to make accessing online data faster.</p>
@@ -27,8 +27,8 @@
         <div class='header-text'>
             <p>I started working with iSTAR, under Dr. Eman Hammad, during the spring 2024 semester doing a combination of IT and web development. I worked on the Cyber Expert Project Team where we designed a website to host virtual labs for students and monitor their engagement. I'm currently researching the viability of implementing trust based network architecture with DAG based blockchains in a project called Trust DAG.</p>
         </div>
-        <div class='project-image-wrapper'>
-            <img src={iSTAR} class='' alt='Structurology'/>
+        <div class='project-image-wrapper experience'>
+            <img src={iSTAR} alt='Structurology'/>
         </div>
     </div>
 
